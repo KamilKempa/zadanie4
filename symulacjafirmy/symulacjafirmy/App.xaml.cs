d52f0207c1fcd25fce130b95c9b65c0d380a33eb
Kamil <chx102418@student.chorzow.merito.pl>
2026-01-28 12:40:19 +0100

    dodanie projektu wpf czystego

---
+++ symulacjafirmy/symulacjafirmy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace symulacjafirmy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
